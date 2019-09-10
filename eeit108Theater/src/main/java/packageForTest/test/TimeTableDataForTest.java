5fb9e7815e7bd46fe4c85930d87eb4738e98961c
黃靖峰 <eeit10815@outlook.com>
2019-09-10 16:25:31 +0800

    insert timeTable for test

---
+++ eeit108Theater/src/main/java/packageForTest/test/TimeTableDataForTest.java
@@ -0,0 +1,55 @@
+package packageForTest.test;
+
+import java.util.*;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.web.entity.MovieBean;
+import com.web.entity.SeatBean;
+import com.web.entity.TimeTableBean;
+
+import packageForTest.util.HibernateUtils;
+
+public class TimeTableDataForTest {
+
+	public static void main(String[] args) {
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			TimeTableBean tb = new TimeTableBean();
+			tb.setAvailable(true);
+			tb.setDuration(120);
+			tb.setStartTime(new Date(1999, 9, 10, 12, 20));
+			tb.setEndTime(new Date(1999, 9, 10, 14, 20));
+			tb.setTheater("A");
+			tb.setVersion("3D");
+			tb.setMovie(session.get(MovieBean.class, 1));
+			tb.setMovieName(tb.getMovie().getMovieName());
+			Set<SeatBean> seats = new HashSet<>();
+			SeatBean sb = new SeatBean();
+			sb.setAvailable(true);
+			sb.setRow("A");
+			sb.setColumn("8");
+			seats.add(sb);
+			sb = new SeatBean();
+			sb.setAvailable(true);
+			sb.setRow("A");
+			sb.setColumn("9");
+			seats.add(sb);
+			tb.setSeats(seats);
+			session.save(tb);
+			tx.commit();
+			System.out.println("========新增timeTable成功============");
+		} catch (Exception e) {
+			tx.rollback();
+			System.out.println("========新增timeTable失敗============");
+			e.printStackTrace();
+		}
+
+	}
+
+}
