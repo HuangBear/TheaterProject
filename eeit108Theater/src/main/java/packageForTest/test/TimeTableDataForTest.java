8aa1ddccc3f593f52de5b3f3db20ae76d19fcbef
陳耀堂 <freedom63301@gmail.com>
2019-09-16 19:50:25 +0800

    ignore不需要之java檔
    可惡的conflict

--- eeit108Theater/src/main/java/packageForTest/test/TimeTableDataForTest.java
+++ eeit108Theater/src/main/java/packageForTest/test/TimeTableDataForTest.java
@@ -23,7 +23,7 @@ public class TimeTableDataForTest {
 			TimeTableBean tb = new TimeTableBean();
 			tb.setAvailable(true);
 			tb.setDuration(120);
-			tb.setStartTime(new Date(1999, 9, 10, 12, 20));
+			tb.setStartTime(new Date(1999, 09, 10, 12, 20));
 			tb.setTheater("A");
 			tb.setVersion("3D");
 			tb.setMovie(session.get(MovieBean.class, 1));
