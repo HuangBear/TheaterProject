d10311fa0e8d4b82f630661f03221ea9ac91e39f
Bear-laptop <eeit10815@outlook.com>
2019-09-25 12:41:40 +0800

    新增方法

--- eeit108Theater/src/main/java/com/web/service/impl/OrderServiceImpl.java
+++ eeit108Theater/src/main/java/com/web/service/impl/OrderServiceImpl.java
@@ -10,76 +10,72 @@ import org.springframework.transaction.annotation.Transactional;
 import com.web.dao.OrderDao;
 import com.web.dao.SeatDao;
 import com.web.entity.OrderBean;
+import com.web.entity.OrderItemBean;
 import com.web.entity.SeatBean;
+import com.web.entity.TimeTableBean;
 import com.web.service.OrderService;
 
 @Transactional
 @Service
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 	@Autowired
 	OrderDao odao;
 	@Autowired
 	SeatDao sdao;
-	
+
 	@Override
 	public int saveOrder(OrderBean order) {
-		
+
 		return odao.saveOrder(order);
 	}
 
-	
 	@Override
 	public int deleteOrderByNo(Integer orderNo) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	
 	@Override
 	public int deleteAll() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	
 	@Override
 	public int updateOrder(OrderBean order) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	
 	@Override
 	public int discontinueAll() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	
 	@Override
 	public int continueAll() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	
 	@Override
 	public OrderBean getOrderByNo(Integer orderNo) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public int setSeatToOrder(OrderBean ob, String[] seats) {
 		int tid = ob.getTimeTable().getNo();
 		System.out.println("tid = " + tid);
 		List<SeatBean> list = new ArrayList<>();
-		for(String rowCol : seats) {
-			String row = rowCol.substring(0,1);
+		for (String rowCol : seats) {
+			String row = rowCol.substring(0, 1);
 			System.out.println("row = " + row);
 			Integer col = Integer.valueOf(rowCol.substring(1));
 			System.out.println("col = " + col);
-			if(sdao.getSeat(tid, row, col) != null) {
+			if (sdao.getSeat(tid, row, col) != null) {
 				return -1;
 			}
 			SeatBean sb = new SeatBean(tid, row, col);
@@ -89,36 +85,54 @@ public class OrderServiceImpl implements OrderService{
 		System.out.println("===setSeatToOrder Result:\n" + list);
 		return seats.length;
 	}
-	
+
 	@Override
 	public List<OrderBean> getOrdersByOwnerId(String memberId) {
 		return odao.getOrdersByOwnerId(memberId);
 	}
 
-	
 	@Override
 	public List<OrderBean> getAllOrders() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
 	@Override
 	public List<OrderBean> getAllAvailable() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
 	@Override
 	public List<OrderBean> getGuestOrders(String ownerEmail, String ownerPhone, Boolean checkStatus) {
-		return odao.getGuestOrders(ownerEmail, ownerPhone, checkStatus);
+		List<OrderBean> list = odao.getGuestOrders(ownerEmail, ownerPhone, checkStatus);
+		for (OrderBean order : list)
+			cancelLazy(order);
+		return list;
 	}
-
 
 	@Override
 	public List<OrderBean> getMemberOrders(String memberId, Boolean checkStatus) {
-		return odao.getMemberOrders(memberId, checkStatus);
+		List<OrderBean> list = odao.getMemberOrders(memberId, checkStatus);
+		for (OrderBean order : list)
+			cancelLazy(order);
+		return list;
+	}
+
+	private void cancelLazy(OrderBean ob) {
+		List<OrderItemBean> oiList = ob.getOrderItems();
+		if (oiList != null)
+			for (OrderItemBean oib : oiList)
+				oib.getAvailable();
+		List<SeatBean> sList = ob.getSeats();
+		if (sList != null)
+			for (SeatBean sb : sList)
+				sb.getAvailable();
+		cancelLazy(ob.getTimeTable());
+	}
+
+	private void cancelLazy(TimeTableBean tb) {
+		tb.getMovie().getAvailable();
 	}
 
 }
