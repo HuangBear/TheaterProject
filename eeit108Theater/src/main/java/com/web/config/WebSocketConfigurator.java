eb2e73aeb9765c6198cf9f8da44a9455e83862ef
劉祈寬 <nossi1970@gmail.com>
2019-09-28 23:03:34 +0800

    WebSocket調整

--- eeit108Theater/src/main/java/com/web/config/WebSocketConfigurator.java
+++ eeit108Theater/src/main/java/com/web/config/WebSocketConfigurator.java
@@ -1,19 +1,30 @@
-package com.web.controller;
+package com.web.config;
 
 import javax.servlet.http.HttpSession;
 import javax.websocket.HandshakeResponse;
 import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerEndpointConfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 import com.web.entity.EmployeeBean;
 import com.web.entity.MemberBean;
+import com.web.service.EmployeeService;
 
+
+@Configuration
+@ComponentScan({"com.web"})
 public class WebSocketConfigurator extends ServerEndpointConfig.Configurator {
-
+	@Autowired
+	EmployeeService serviceEmp;
+	
 	@Override
 	public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+		
 		sec.getUserProperties().put("username", "M" + "訪客");
-
+		
 		if (((HttpSession) request.getHttpSession()).getAttribute("LoginOK") != null) {
 			String allClassName = ((HttpSession) request.getHttpSession()).getAttribute("LoginOK").getClass()
 					.toString();
@@ -31,6 +42,7 @@ public class WebSocketConfigurator extends ServerEndpointConfig.Configurator {
 			
 			if ("EmployeeBean".equals(className)) {
 				EmployeeBean loginToken = (EmployeeBean) ((HttpSession) request.getHttpSession()).getAttribute("employeeBean1");
+				//System.out.println(serviceEmp.checkEmpEmail(loginToken.getEmail())); 
 				sec.getUserProperties().put("username",
 						"E" +  loginToken.getPermission()+  loginToken.getName());
 			} else {
